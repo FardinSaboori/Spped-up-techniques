d19824a3c96ae7491e2865eb2f1a3ee96dc23096
fardin sb <Fardin.Saboori@edu.sait.ca>
2020-06-22 18:05:39 -0600

    first commit

---
+++ chita.py
@@ -0,0 +1,31 @@
+import timeit
+import numexpr as ne
+import pandas as pd
+code_to_test = """
+import numpy as np
+from timeit import Timer
+import numexpr as ne
+
+
+def haversine(lat1, lon1, lat2, lon2):
+    radius = 100
+    lat1, lon1, lat2, lon2 = map(np.deg2rad, [lat1, lon1, lat2, lon2])
+
+    dlat = lat2 - lat1
+    dlon = lon2 - lon1
+    # typical expression writings: f = np.sin(dlat/2)**2 + ne.evaluate("cos(lat1)") * np.cos(lat2) * np.sin(dlon/2)**2
+    f = ne.evaluate("sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2")
+    g = 2 * np.arcsin(np.sqrt(f))
+
+    distance = radius * g
+    print(distance)
+    return distance
+
+lat2 = np.array(range(100000))
+lon2 = np.array(range(100000))
+
+
+haversine(65, 70, lat2, lon2)
+"""
+
+print(timeit.timeit(code_to_test, number=100)/100)
